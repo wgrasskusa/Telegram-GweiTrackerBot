222f8c409fc539321e0c71c2e0456fb4225c2791
izu14 <imfauzan14@gmail.com>
2021-03-29 19:07:52 +0700

    a little bit speed-up
    use requests.Session instead requests

--- bot.py
+++ bot.py
@@ -9,13 +9,14 @@ from telegram.ext import MessageHandler, Filters, CommandHandler, Updater
 
 updater = Updater(token='PUTYOURTELEGRAMBOTTOKENHERE', use_context=True)
 dispatcher = updater.dispatcher
+session = requests.Session()
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 def tracker():
     #GasTracker
     gas_url = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=PUTYOURETHERSCANAPIKEYHERE'
-    gas_response = requests.get(gas_url)
+    gas_response = session.get(gas_url)
     gas_data = json.loads(gas_response.text)
     gas_result = gas_data["result"]
     tracker.gas_low = gas_result["SafeGasPrice"]
@@ -23,19 +24,19 @@ def tracker():
     tracker.gas_high = gas_result["FastGasPrice"]
     #PriceUSD
     peth_url = 'https://api.etherscan.io/api?module=stats&action=ethprice&apikey=PUTYOURETHERSCANAPIKEYHERE'
-    peth_response = requests.get(peth_url)
+    peth_response = session.get(peth_url)
     peth_data = json.loads(peth_response.text)
     peth_result = peth_data["result"]
     pethr = peth_result["ethusd"]
     tracker.peth = format_currency(pethr, 'USD', locale="en_GB")
     paxs_url = 'https://api.coingecko.com/api/v3/simple/price?ids=axie-infinity&vs_currencies=usd'
-    paxs_response = requests.get(paxs_url)
+    paxs_response = session.get(paxs_url)
     paxs_data = json.loads(paxs_response.text)
     paxs_result = paxs_data["axie-infinity"]
     paxsr = paxs_result["usd"]
     tracker.paxs = format_currency(paxsr, 'USD', locale="en_GB")
     pslp_url = 'https://api.coingecko.com/api/v3/simple/price?ids=small-love-potion&vs_currencies=usd'
-    pslp_response =  requests.get(pslp_url)
+    pslp_response =  session.get(pslp_url)
     pslp_data = json.loads(pslp_response.text)
     pslp_result = pslp_data["small-love-potion"]
     pslpr = pslp_result["usd"]
@@ -45,37 +46,37 @@ def tracker():
     tgas_url2 = '&apikey=PUTYOURETHERSCANAPIKEYHERE'
     tlgas_wei = str(tracker.gas_low)+'000000000'
     tlgas_url = tgas_url1 + tlgas_wei + tgas_url2 
-    tlgas_response =  requests.get(tlgas_url)
+    tlgas_response =  session.get(tlgas_url)
     tlgas_data = json.loads(tlgas_response.text)
     tlgas_s = tlgas_data["result"]
     tracker.tlgas_sec = int(tlgas_s)
     tagas_wei = str(tracker.gas_avg)+'000000000'
     tagas_url = tgas_url1 + tagas_wei + tgas_url2 
-    tagas_response =  requests.get(tagas_url)
+    tagas_response =  session.get(tagas_url)
     tagas_data = json.loads(tagas_response.text)
     tagas_s = tagas_data["result"]
     tracker.tagas_sec = int(tagas_s)
     thgas_wei = str(tracker.gas_high)+'000000000'
     thgas_url = tgas_url1 + thgas_wei + tgas_url2 
-    thgas_response =  requests.get(thgas_url)
+    thgas_response =  session.get(thgas_url)
     thgas_data = json.loads(thgas_response.text)
     thgas_s = thgas_data["result"]
     tracker.thgas_sec = int(thgas_s)
     #PriceIDR
     pethid_url = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=idr'
-    pethid_response = requests.get(pethid_url)
+    pethid_response = session.get(pethid_url)
     pethid_data = json.loads(pethid_response.text)
     pethid_result = pethid_data["ethereum"]
     pethidr = pethid_result["idr"]
     tracker.pethid = format_currency(pethidr, 'IDR', locale="id_ID")
     paxsid_url = 'https://api.coingecko.com/api/v3/simple/price?ids=axie-infinity&vs_currencies=idr'
-    paxsid_response = requests.get(paxsid_url)
+    paxsid_response = session.get(paxsid_url)
     paxsid_data = json.loads(paxsid_response.text)
     paxsid_result = paxsid_data["axie-infinity"]
     paxsidr = paxsid_result["idr"]
     tracker.paxsid = format_currency(paxsidr, 'IDR', locale="id_ID")
     pslpid_url = 'https://api.coingecko.com/api/v3/simple/price?ids=small-love-potion&vs_currencies=idr'
-    pslpid_response =  requests.get(pslpid_url)
+    pslpid_response =  session.get(pslpid_url)
     pslpid_data = json.loads(pslpid_response.text)
     pslpid_result = pslpid_data["small-love-potion"]
     pslpidr = pslpid_result["idr"]
@@ -142,9 +143,3 @@ def prices(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=mprice, parse_mode=ParseMode.MARKDOWN)
 prices_handler = CommandHandler('prices', prices)
 dispatcher.add_handler(prices_handler)
-
-#import schedule, time
-#schedule.every(1).seconds.do(tracker)
-#while 1:
-#    schedule.run_pending()
-#    time.sleep(1)
